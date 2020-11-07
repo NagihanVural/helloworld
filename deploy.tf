d9f959a17d6ef9d882d2e44a99bfd5d656484576
Nagihan Vural <nagihan.vural@yandex.com>
2020-11-07 17:16:02 +0300

    Update deploy.tf

--- deploy.tf
+++ deploy.tf
@@ -126,7 +126,7 @@ resource "aws_ecs_service" "nagi_demo_service" {
   cluster         = aws_ecs_cluster.nagi_demo_cluster.id         # Referencing our created Cluster
   task_definition = aws_ecs_task_definition.nagi_demo_task.arn # Referencing the task our service will spin up
   launch_type     = "FARGATE"
-  desired_count   = 3 # Setting the number of containers to 3
+  desired_count   = 1 # Setting the number of containers to 3
 
   load_balancer {
     target_group_arn = aws_lb_target_group.target_group.arn # Referencing our target group
